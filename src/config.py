890e590992b61e3753a80ae6bcf64e6530d7687c
Martin Hertach BLV <martin.hertach@blv.admin.ch>
2025-07-16 16:46:11 +0200

    Update config.py

--- src/config.py
+++ src/config.py
@@ -10,8 +10,8 @@ HARVEST_API_URL = "https://www.studydata.blv.admin.ch/api/catalog/"
 # I14Y API configuration
 ###########################################
 
-# API_BASE_URL = "https://api.i14y.admin.ch/api/partner/v1/" # Prod environement 
-API_BASE_URL = "https://api-a.i14y.admin.ch/api/partner/v1/" # ABN enironement 
+API_BASE_URL = "https://api.i14y.admin.ch/api/partner/v1/" # Prod environement 
+# API_BASE_URL = "https://api-a.i14y.admin.ch/api/partner/v1/" # ABN enironement 
 API_TOKEN = f"Bearer {os.environ['ACCESS_TOKEN']}" 
 
 # Organization settings
